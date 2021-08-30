859f563bb8f490b77d23b5a1f35ec40ff498b949
David <DEvangelista54@gmail.com>
2021-08-30 17:22:29 -0400

    commit tree 22948e826e5a84d5588757ef6cb2868192ae97ef

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+//variables
+let num1 = 31;
+let num2 = 2;
+let num3 = 20;
+let num4 = 5;
+
+//multiply
+let multiply = num1 * num2;
+
+//random
+let random = Math.floor(Math.random() + 1);
+
+//mod
+
+let mod = num3 / num4;
+
+//max
+let max = Math.max(1,2,5,15,20);
